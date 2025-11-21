28d3e0447b2236cde4e9b1a24ab177ccdeacc5c0
goalizc <goalizc@qq.com>
2025-11-21 09:14:22 +0800

    Hungarian放入单独文件

---
+++ src/modules/sbstudio/api/hungarian.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+class Hungarian:
+    def __init__(self, weights):
+        weights = -np.array(weights).astype(np.float32)
+        self.weights = weights
+        self.n, self.m = weights.shape
+        assert self.n <= self.m
+        self.label_x = np.max(weights, axis=1)
+        self.label_y = np.zeros((self.m, ), dtype=np.float32)
+        self.max_match = 0
+        self.xy = -np.ones((self.n,), dtype=int)
+        self.yx = -np.ones((self.m,), dtype=int)
+
+    def do_augment(self, x, y):
+        self.max_match += 1
+        while x != -2:
+            self.yx[y] = x
+            ty = self.xy[x]
+            self.xy[x] = y
+            x, y = self.prev[x], ty
+
+    def find_augment_path(self):
+        self.S = np.zeros((self.n,), bool)
+        self.T = np.zeros((self.m,), bool)
+        self.slack = np.zeros((self.m,), dtype=np.float32)
+        self.slackyx = -np.ones((self.m,), dtype=int)
+        self.prev = -np.ones((self.n,), int)
+
+        queue, st = [], 0
+        root = -1
+
+        for x in range(self.n):
+            if self.xy[x] == -1:
+                queue.append(x);
+                root = x
+                self.prev[x] = -2
+                self.S[x] = True
+                break
+
+        self.slack = self.label_y + self.label_x[root] - self.weights[root]
+        self.slackyx[:] = root
+
+        while True:
+            while st < len(queue):
+                x = queue[st]; st+= 1
+
+                is_in_graph = np.isclose(self.weights[x], self.label_x[x] + self.label_y)
+                nonzero = np.nonzero(np.logical_and(is_in_graph, np.logical_not(self.T)))[0]
+
+                for y in nonzero:
+                    if self.yx[y] == -1:
+                        return x, y
+                    self.T[y] = True
+                    queue.append(self.yx[y])
+                    self.add_to_tree(self.yx[y], x)
+
+            self.update_labels()
+            queue, st = [], 0
+            is_in_graph = np.isclose(self.slack, 0)
+            nonzero = np.nonzero(np.logical_and(is_in_graph, np.logical_not(self.T)))[0]
+
+            for y in nonzero:
+                x = self.slackyx[y]
+                if self.yx[y] == -1:
+                    return x, y
+                self.T[y] = True
+                if not self.S[self.yx[y]]:
+                    queue.append(x)
+                    self.add_to_tree(self.yx[y], x)
+
+    def compute(self):
+        while self.max_match < self.n:
+            x, y = self.find_augment_path()
+            self.do_augment(x, y)
+        return range(self.n), self.xy
+
+    def add_to_tree(self, x, prevx):
+        self.S[x] = True
+        self.prev[x] = prevx
+        better_slack_idx = self.label_x[x] + self.label_y - self.weights[x] < self.slack
+        self.slack[better_slack_idx] = self.label_x[x] + \
+            self.label_y[better_slack_idx] - self.weights[x, better_slack_idx]
+        self.slackyx[better_slack_idx] = x
+
+    def update_labels(self):
+        delta = self.slack[np.logical_not(self.T)].min()
+        self.label_x[self.S] -= delta
+        self.label_y[self.T] += delta
+        self.slack[np.logical_not(self.T)] -= delta
